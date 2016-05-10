4e7c9162ce33dc0002d80392940f5431ebf0c1f7
Nate Clark <n8@n8io.com>
2016-05-10 09:34:17 -0400

    FQP'd the docker-compose path

--- cycle-docker.sh
+++ cycle-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DOCKER_COMPOSE_BIN="/usr/local/bin/docker-compose"
 cd /code/media-server-config
-docker-compose restart > /root/diagnostics/last-cycle-docker.log
+${DOCKER_COMPOSE_BIN} restart > /root/diagnostics/last-cycle-docker.log 2>&1
 echo $(date +"%Y-%m-%d %r") >> /root/diagnostics/last-cycle-docker.log
